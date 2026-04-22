db3a7cc524c701bff57bf9c9ccd91ca134a08f6f
franczsy <j.francisco.552788@umindanao.edu.ph>
2026-04-22 00:37:02 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/product');
 });
 
 Route::get('/dashboard', function () {
